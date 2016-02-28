2ddf320806a0b75d8c3409940b4a6ed186008309
John McGuigan <jrm5100@gmail.com>
2016-02-28 15:06:12 -0500

    Added legends

--- js/visualization.js
+++ js/visualization.js
@@ -32,8 +32,6 @@ function draw(data) {
         .rollup(agg_years)
         .entries(data);
 
-    console.log(data_nested);
-
     var margin = {top: 10, right: 10, bottom: 50, left: 80},
         width = 950 - margin.left - margin.right,
         height = 450 - margin.top - margin.bottom,
@@ -52,12 +50,14 @@ function draw(data) {
     //xScale for RA
     var yScale_RA = d3.scale.linear()
         .range([0, (height/2)-middlespacing])
-        .domain([0, d3.max(data_nested, function(d){return d.values['min_RA']})]);    
+        .domain([0, d3.max(data_nested, function(d){return d.values['max_RA']})]);    
 
     //color by league
+    var colors = ["#98abc5", "#8a89a6"]
+    var leagues = ["AL", "NL"]
     var color = d3.scale.ordinal()
-        .range(["#98abc5", "#8a89a6"]) //fix color later
-        .domain(['AL', 'NL'])
+        .range(colors)
+        .domain(leagues);
 
     var xAxis = d3.svg.axis()
         .scale(xScale)
@@ -84,7 +84,7 @@ function draw(data) {
         .selectAll("text") //rotate tick labeels and center them between the graphs
         .attr("y", 0)
         .attr("x", 0)
-        .attr("dy", ".35em") //center in bars
+        .attr("dy", ".5em") //center in bars
         .attr("transform", "rotate(90)")
         .style("text-anchor", "middle")
         .classed("short-season", function(d){return ['1972', '1981', '1994', '1995'].indexOf(d)>-1;}); //add a class if the season was strike-shortened
@@ -93,6 +93,7 @@ function draw(data) {
         .attr("class", "y axis")
         .call(yAxis_R)
         .append("text")
+        .attr("class", "label")
         .style("text-anchor", "middle")
         .attr("x", -height/4)
         .attr("y", -50)
@@ -104,38 +105,113 @@ function draw(data) {
         .attr("transform", "translate(0," + (height/2 +middlespacing) + ")")
         .call(yAxis_RA)
         .append("text")
+        .attr("class", "label")
         .style("text-anchor", "middle")
         .attr("x", -height/4)
         .attr("y", -50)
         .attr("transform", "rotate(-90)")
         .text("Fewest Runs Allowed");
 
-    var Rbars = svg.append("g")
-        .attr("id", "Rbar-group")
-        .selectAll(".Rbar")
+    var barwidth = xScale.rangeBand()-2;
+    var median_marker_height = 4
+
+    //Plot Run Data
+    var chart_R = svg.append("g").attr("id", "chart_R")
+        
+    chart_R.selectAll(".Rbar")
         .data(data_nested, function(d){return d['key'];}) //key is the year
         .enter()
-            .append("rect")
+        .append("rect")
             .attr("class", "Rbar")
             .attr("x", function(d){return xScale(+d['key']);})
             .attr("y", function(d){return yScale_R(d.values['max_R']);})//top of rect
-            .attr("width", xScale.rangeBand()-2) //set bar width to the max minus some spacing
+            .attr("width", barwidth)
             .attr("height", function(d){return yScale_R.range()[0] - yScale_R(d.values['max_R']);}) //dist between y value and bottom
             .attr("fill", function(d){return color(d.values['max_R_league']);});
 
-    var RAbars = svg.append("g")
-        .attr("id", "RAbar-group")
+    chart_R.selectAll(".median-marker")
+        .data(data_nested, function(d){return d['key'];}) //key is the year
+        .enter()
+        .append("rect")
+            .attr("class", "median-marker")
+            .attr("x", function(d){return xScale(d['key']);})
+            .attr("y", function(d){return yScale_R(d.values['median_R']);})
+            .attr("width", barwidth)
+            .attr("height",median_marker_height);
+
+    var chart_RA = svg.append("g").attr("id", "chart_RA")
         .attr("transform", "translate(0," + (height/2 +middlespacing) + ")")
+        
+    chart_RA
         .selectAll(".RAbar")
         .data(data_nested, function(d){return d['key'];}) //key is the year
         .enter()
         .append("rect")
             .attr("class", "RAbar")
             .attr("x", function(d){return xScale(+d['key']);})
-            .attr("y", yScale_RA.range()[0])
-            .attr("width", xScale.rangeBand()-2) //set bar width to the max minus some spacing
+            .attr("y", 0)
+            .attr("width", barwidth)
             .attr("height", function(d){return yScale_RA(d.values['min_RA']);})
             .attr("fill", function(d){return color(d.values['min_RA_league']);});
+
+    chart_RA.selectAll(".median-marker")
+        .data(data_nested, function(d){return d['key'];}) //key is the year
+        .enter()
+        .append("rect")
+            .attr("class", "median-marker")
+            .attr("x", function(d){return xScale(d['key']);})
+            .attr("y", function(d){return yScale_RA(d.values['median_RA']);})
+            .attr("width", barwidth)
+            .attr("height", median_marker_height);
+
+    //Add legend at the bottom
+    var legendtext = leagues.map(function(l){return "Team was from the " + l;}).concat(['Median Value']),
+        legendcolors = colors.concat(["#000000"]);
+
+    var legend = svg.append("g")
+        .attr("class", "legend")
+        .attr("height", 70) //20px for each with 5px spacing
+        .attr("width", 200)
+        .attr("transform", "translate(" + (width + margin.right - 200) + "," + (height + margin.bottom - 70)+ ")"); //bottom right corner
+
+    legend.selectAll("rect")
+        .data(legendcolors)
+        .enter()
+        .append("rect")
+        .attr("x", 0)
+        .attr("y", function(d, i) {
+            if(d=="#000000"){return i * 25 + 8;} //different pos for median rect b/c it is shorter
+            else {return i * 25;};})
+        .attr("width", 20)
+        .attr("height", function(d){
+            if(d=="#000000"){return median_marker_height;} //median rect is shorter
+            else {return 20;};})
+        .attr("fill", function(d) {return d;});
+
+    legend.selectAll("text")
+        .data(legendtext)
+        .enter()
+        .append("text")
+        .text(function(d){return d;})
+        .attr("x", 25)
+        .attr("y", function(d, i) {return i * 25 + 15;});
+
+    //x-axis label
+    svg.append("text")
+        .attr("class", "label")
+        .attr("x", width/2)
+        .attr("y", height + 10)
+        .attr("text-anchor", "middle")
+        .text("Season");    
+
+    //note on strike seasons
+    svg.append("text")
+        .attr("font-size", "1em")
+        .attr("x", width/2)
+        .attr("y", height + 25)
+        .attr("text-anchor", "middle")
+        .attr("fill", "red")
+        .text("*Short season due to strikes");
 
 };
 
